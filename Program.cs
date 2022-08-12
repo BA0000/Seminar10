732cb70e0868ba3a96d41bb967ae9d596fab25bf
Gunter <kaiz@yandex.ru>
2022-08-12 15:29:25 +0300

    Seminar 10 end

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿
+
+// Не успеваю первую задачу доделать (и понять :) ). Вторую сделал. 
+
+// Задача 1: Задайте массив строк. Напишите программу, которая считает кол-во слов в массиве, начинающихся на гласную букву.
+// Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
+
+// int VowelLetter(string[] words, )
+
+
+// string[] words = {"YellowStone", "Shark", "Empty", "Snake", "Infantry"};
+
+// char[] box = {'a', 'e', 'i', 'о', 'u', 'y'};
+
+
+// Console.WriteLine(words);
+
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+// Задача 2: Задайте массив строк. Напишите программу, которая генерирует новый массив, объединяя элементы исходного массива попарно.
+// Пример: { "qwe", "wer", "ert", "rty", "tyu", "yui"} -> { "qwewer", "ertrty", "tyuyui"}
+
+
+
+// string[] MakeStrArray(int number)
+// {
+//     string[] array = new string[number];
+//     for(int i = 0; i < number; i++)
+//     {
+//         Console.Write($"Input a {i+1} word: ");
+//         array[i] = Console.ReadLine();
+//     }
+//     return array;
+// } 
+
+// void SeeStrArray(string[] array) 
+// {
+//     for(int i = 0; i < array.Length; i++)
+//     {
+//         Console.WriteLine(array[i] + " ");
+//     }
+// }
+
+// string[] SplitArray(string[] array1, string[] array2)
+// {
+//     string[] newArray = new string[array1.Length];
+//     for(int i = 0; i < array1.Length; i++)
+//     {
+//         newArray[i] = array1[i] + array1[i];
+//     }
+//     return newArray;
+
+// }
+// string[] myArray1 = MakeStrArray(2);
+// string[] myArray2 = MakeStrArray(2);
+// string[] myArray3 = SplitArray(myArray1,myArray2);
+// SeeStrArray(myArray1);
+// SeeStrArray(myArray2);
+// SeeStrArray(myArray3);
+
+
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+// -------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
